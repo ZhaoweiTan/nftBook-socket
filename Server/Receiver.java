2cc5cfec2d37350e22358218c08c2694762566a6
Zhaowei Tan <tanzw94@gmail.com>
2017-05-22 18:13:20 -0700

    add server code

---
+++ Server/Receiver.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Receiver {
+
+    public static void main(String[] args) {
+        int port = 48010;
+        new Receiver().run(port);
+    }
+
+    public void run(int port) {    
+      try {
+        DatagramSocket serverSocket = new DatagramSocket(port);
+        byte[] receiveData = new byte[100];
+     
+        DatagramPacket receivePacket = new DatagramPacket(receiveData,
+                           receiveData.length);
+        System.out.println("Start Listening");
+
+        while(true)
+        {
+              serverSocket.receive(receivePacket);
+              String sentence = new String( receivePacket.getData(), 0,
+                                 receivePacket.getLength() );
+              System.out.println("RECEIVED: " + sentence);
+              
+              // now send acknowledgement packet back to sender     
+              InetAddress IPAddress = receivePacket.getAddress();
+              String sendString = "ACK";
+              byte[] sendData = sendString.getBytes();
+              System.out.println(IPAddress.toString() +  Integer.toString(receivePacket.getPort()));
+              DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,
+                   IPAddress, receivePacket.getPort());
+              serverSocket.send(sendPacket);
+        }
+      } catch (IOException e) {
+              System.out.println(e);
+      }
+      // should close serverSocket in finally block
+    }
+}
